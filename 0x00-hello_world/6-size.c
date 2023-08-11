44fa64d9b0fce5d3e343a44d853b78b7cbdb6f53
zopa77 <zeynoba00@gmail.com>
2023-08-11 12:22:53 -0700

    this is sizing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
  /**
-  * main - this function is main
+  * main -  function is main caluclate the size
   *
-  * Return: always(0)
+  * Return: always 0
   */
 int main(void)
 {
