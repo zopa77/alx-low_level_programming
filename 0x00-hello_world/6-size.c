2fd8d67d8f8a957edc5c5a0629e3c1825331ed5a
zopa77 <zeynoba00@gmail.com>
2023-08-11 11:50:08 -0700

    my size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
  /**
-  * main - this function print type of my machin
+  * main - this function is main
   *
-  * Return:zero
+  * Return: always(0)
   */
 int main(void)
 {
-	printf("size of a char: %lu byte(s)", sizeof(char));
-	printf("size of an int: %lu byte(s)", sizeof(int));
-	printf("size of a long int: %lu byte(s)", sizeof(long int));
-	printf("size of long long int: %lu byte(s)", sizeof(long long int));
-	printf("size of float: %lu byte(S)", sizeof(float));
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of float: %lu byte(S)\n", sizeof(float));
 	return (0);
 }
 
