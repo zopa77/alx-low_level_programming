b99a9e0892b641c860adb9c5551c8ced3c455c29
zopa77 <zeynoba00@gmail.com>
2023-08-11 06:02:49 -0700

    my first c programing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - is typing a title
+ *
+ * Return: always typing 0
+ */
+
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle);
+	return (0);
+}
