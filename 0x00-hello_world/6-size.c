e66306e854c0b239a00fbccc2fa31a430569450f
zopa77 <zeynoba00@gmail.com>
2023-08-11 12:36:55 -0700

    my sizing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+
  /**
   * main -  function is main caluclate the size
   *
   * Return: always 0
   */
+
 int main(void)
 {
-	printf("size of a char: %lu byte(s)\n", sizeof(char));
-	printf("size of an int: %lu byte(s)\n", sizeof(int));
-	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("size of float: %lu byte(S)\n", sizeof(float));
+
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(S)\n", sizeof(float));
 	return (0);
+
 }
 
