fe0ac8ad6944add89006bad6afeaa65d8d0eff79
zopa77 <zeynoba00@gmail.com>
2023-08-11 14:01:31 -0700

    typing with write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - this program print something
+ *main - this program print something
  *
- * Return: always return 1
+ *Return: always return 1
  */
 int main(void)
 {
-            write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+
 }
